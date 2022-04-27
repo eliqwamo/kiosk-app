ae698870db69ef0a781c1b9c04e6688ea615ba3a
giggsi-bs <asaf@giggsi.co.il>
2022-04-27 12:17:17 +0300

    27042022

--- src/screens/dashboard/index.js
+++ src/screens/dashboard/index.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import {View,Text} from 'react-native';
+import React, { useEffect, useState, useCallback } from "react";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import * as storeActions from "../../../store/action";
+import Colors from "../../utilis/AppColors";
 
-const Dashboard = props => {
-    return(
-        <View><Text>Dashboard</Text></View>
-    )
-}
+const Dashboard = (props) => {
+  const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getAllStores = useCallback(async () => {
+    let action = storeActions.get_stores_action();
+    setIsLoading(true);
+    try {
+      await dispatch(action);
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+    }
+  }, [setIsLoading, dispatch, storeActions.get_stores_action]);
+
+  useEffect(() => {
+    getAllStores();
+  }, [getAllStores]);
+
+  const allStores = useSelector((state) => state.allStores);
+
+
+
+  return (
+    <View>
+      {isLoading ? (
+        <ActivityIndicator size="large" color={Colors.happy_green} />
+      ) : (
+        <Text>Stores</Text>
+      )}
+    </View>
+  );
+};
 
 export default Dashboard;
--- src/screens/stores/index.js
+++ src/screens/stores/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import {View,Text} from 'react-native';
+import React, {useEffect,useState,useCallback} from 'react';
+import {View, Text, FlatList, ActivityIndicator} from 'react-native';
+import { useDispatch, useSelector } from "react-redux";
 
 const Stores = props => {
+
+    const allStores = useSelector((state) => state.allStores);
+    console.log('NEW STORES: ' + JSON.stringify(allStores));
+    console.log('NEW STORES LENGTH: ' + JSON.stringify(allStores.allStores.stores.length));
+
     return(
-        <View><Text>Stores</Text></View>
+        <View>
+            {
+                allStores.allStores.stores.length > 0 ? (
+                    <Text>111</Text>
+                ) : (
+                    <Text>222</Text>
+                )
+            }
+        </View>
     )
 }
 
